21fa9d66a60fa84c0a980c3ace7f8674664468ec
filipe <filipe11402@outlook.pt>
2021-06-27 17:04:17 +0100

    Creating and updating ManyToMany

--- Employee_Stack/Employee_Stack/Controllers/EmployeeController.cs
+++ Employee_Stack/Employee_Stack/Controllers/EmployeeController.cs
@@ -24,7 +24,7 @@ namespace Employee_Stack.Controllers
         public IActionResult Index()
         {
 
-            var EmpList = _db.Employees;
+            var EmpList = _db.Employees.Include(x => x.TeckStack);
 
 
             return View(EmpList);
@@ -33,23 +33,37 @@ namespace Employee_Stack.Controllers
         //GET method
         public IActionResult Create() 
         {
+            var newEmployee = new Employee();
+            newEmployee.TeckStack = new List<TechStack>();
+            PopulateLanguageData(newEmployee);
 
-            //create private function that creates object in VM and define True/False based at what languages are known by that employee
 
             return View();
         }
 
+        //POST method
         [HttpPost]
-        public IActionResult CreatePost(EmployeeTechStackViewModel newEmployee)
+        public IActionResult CreatePost(Employee newEmployee, string[] selectedLanguages) 
         {
+            if (selectedLanguages != null) 
+            {
+                newEmployee.TeckStack = new List<TechStack>();
+
+                foreach (var language in selectedLanguages) 
+                {
+                    var languageToAdd = _db.TeckStack.Find(int.Parse(language));
+                    newEmployee.TeckStack.Add(languageToAdd);
+                }
+            }
 
             if (ModelState.IsValid) 
             {
-                _db.Add(newEmployee);
+                _db.Employees.Add(newEmployee);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
+            PopulateLanguageData(newEmployee);
             return View();
         }
 
@@ -73,6 +87,37 @@ namespace Employee_Stack.Controllers
             return View(employee);
         }
 
+        //POST method
+        [HttpPost]
+        public IActionResult EditPost(int? Id, string[] selectedLanguages)
+        {
+
+            if (Id == null || Id <= 0)
+            {
+                return NotFound();
+            }
+
+            Employee employeeToUpdate = _db.Employees.Include(x => x.TeckStack).Where(x => x.Id == Id).Single();
+
+            
+            if (employeeToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            UpdateLanguageData(selectedLanguages, employeeToUpdate);
+
+            if (ModelState.IsValid) 
+            {
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            
+
+            return View(employeeToUpdate);
+        }
+
         //Populating data in viewmodel to show checkboxes/checked checkboxes
         private void PopulateLanguageData(Employee employee) 
         {
@@ -92,6 +137,36 @@ namespace Employee_Stack.Controllers
             }
 
             ViewBag.Languages = viewModel;
+        }
+
+        private void UpdateLanguageData(string[] selectedLanguages, Employee employeeToUpdate) 
+        {
+            if (selectedLanguages == null) 
+            {
+                employeeToUpdate.TeckStack = new List<TechStack>();
+            }
+
+            //creating hashset to retrieve what languages where selected and the one that already exists
+            var selectedLanguagesHS = new HashSet<String>(selectedLanguages);
+            var currentEmployeeLanguagesHS = new HashSet<int>(employeeToUpdate.TeckStack.Select(x => x.Id));
+
+            foreach(var language in _db.TeckStack) 
+            {
+                if (selectedLanguagesHS.Contains(language.Id.ToString()))
+                {
+                    if (!currentEmployeeLanguagesHS.Contains(language.Id))
+                    {
+                        employeeToUpdate.TeckStack.Add(language);
+                    }
+                }
+                else 
+                {
+                    if (currentEmployeeLanguagesHS.Contains(language.Id)) 
+                    {
+                        employeeToUpdate.TeckStack.Remove(language);
+                    }
+                }
+            }
         }
     }
 }
